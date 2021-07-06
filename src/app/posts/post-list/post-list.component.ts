598391cff25aae2d3f6e4fec2a03f107457b3ecf
yelin-gitple <yelin-gitple@gitple.com>
2021-07-06 17:28:52 +0900

    Structural Directives - ngIf, ngFor

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Post {
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent {
-  posts = [
-    { title: 'First Post', content: "This it the first post's content" },
-    { title: 'Second Post', content: "This it the second post's content" },
-    { title: 'Third Post', content: "This it the third post's content" },
-  ];
+  // posts = [
+  //   { title: 'First Post', content: "This is the first post's content" },
+  //   { title: 'Second Post', content: "This is the second post's content" },
+  //   { title: 'Third Post', content: "This is the third post's content" },
+  // ];
+  posts:Post[] = [];
 }
