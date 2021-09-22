d4a4e0a379d4ea8e103afe88ac3ceb323fa4f239
iylinkim <kiylin94@naver.com>
2021-09-22 18:39:34 +0900

    #40_Understanding CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requsted-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
@@ -16,8 +29,8 @@ app.use("/api/posts", (req, res, next) => {
     },
   ];
   res.status(200).json({
-      messge: "Posts fetched succesfully",
-      posts
+    messge: "Posts fetched succesfully",
+    posts,
   });
 });
 
